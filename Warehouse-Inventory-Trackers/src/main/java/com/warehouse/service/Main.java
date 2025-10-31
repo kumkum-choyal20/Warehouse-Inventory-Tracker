bed7779656019541e98a10163f61a7a64ad09d48
KUMKUM CHOYAL <kumkumchoyal@gmail.com>
2025-10-31 13:41:44 +0530

    Main.java

--- Warehouse-Inventory-Trackers/src/main/java/com/warehouse/service/Main.java
+++ Warehouse-Inventory-Trackers/src/main/java/com/warehouse/service/Main.java
@@ -6,28 +6,18 @@ import com.warehouse.observer.AlertService;
 public class Main {
 	public static void main(String[] args) {
 		Warehouse warehouse = new Warehouse();
-
-		// Load saved data (if exists)
 		warehouse.loadFromFile();
-
-		// Attach observer (alert service)
 		warehouse.addObserver(new AlertService());
-
-		// Add some products
 		warehouse.addProduct(new Product("P001", "Laptop", 0, 5));
 		warehouse.addProduct(new Product("P002", "Monitor", 2, 3));
-
-		// Receive shipments
 		warehouse.receiveShipment("P001", 10);
 		warehouse.receiveShipment("P002", 5);
-
 		warehouse.showInventory();
 
 		// Fulfill some orders
 		warehouse.fulfillOrder("P001", 6); // Will trigger low stock alert
 		warehouse.fulfillOrder("P002", 3);
-
-		warehouse.showInventory();
+        warehouse.showInventory();
 
 		// Save inventory state
 		warehouse.saveToFile();
