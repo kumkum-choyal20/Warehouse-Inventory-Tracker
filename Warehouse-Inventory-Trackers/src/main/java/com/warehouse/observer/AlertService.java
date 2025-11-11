6d97113847d335fbb2ea2ceb12223436319fdb5a
KUMKUM CHOYAL <kumkumchoyal@gmail.com>
2025-11-11 15:34:57 +0530

    AlertService.java

--- Warehouse-Inventory-Trackers/src/main/java/com/warehouse/observer/AlertService.java
+++ Warehouse-Inventory-Trackers/src/main/java/com/warehouse/observer/AlertService.java
@@ -5,9 +5,8 @@ import com.warehouse.model.Product;
 public class AlertService implements StockObserver {
 
 	@Override
-	public void onLowStock(Product product) {
-		System.out.println("  Restock Alert: Low stock for " + product.getName() + " – only " + product.getQuantity()
-		+ " left (Threshold: " + product.getReorderThreshold() + ")");
-		
-	}
+	 public void onLowStock(String warehouseName, Product product, int currentQuantity) {
+        System.out.println("ALERT: Low stock in " + warehouseName + 
+                " for " + product.getName() + " – only " + currentQuantity + " left!");
+    }
 }
