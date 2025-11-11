b1fbc499a15174f198f26a198a652027a9b688e7
KUMKUM CHOYAL <kumkumchoyal@gmail.com>
2025-11-11 15:32:54 +0530

    Product.java

--- Warehouse-Inventory-Trackers/src/main/java/com/warehouse/model/Product.java
+++ Warehouse-Inventory-Trackers/src/main/java/com/warehouse/model/Product.java
@@ -1,48 +1,22 @@
 package com.warehouse.model;
 
 public class Product {
-	private final String productId;
-	private String name;
-	private int quantity;
-	private int reorderThreshold;
+	private String id;
+    private String name;
 
-	public Product(String productId, String name, int quantity, int reorderThreshold) {
-		this.productId = productId;
-		this.name = name;
-		this.quantity = quantity;
-		this.reorderThreshold = reorderThreshold;
+    public Product(String id, String name)
+	{
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId()
+	{
+		return id;
 	}
-
-	public String getProductId() {
-		return productId;
-	}
-
-	public String getName() {
+    public String getName()
+	{ 
 		return name;
 	}
 
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public int getReorderThreshold() {
-		return reorderThreshold;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public void setReorderThreshold(int reorderThreshold) {
-		this.reorderThreshold = reorderThreshold;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("[%s] %s | Qty: %d | Threshold: %d", productId, name, quantity, reorderThreshold);
-	}
 }
